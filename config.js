246d56639bea77eb2052278dd92f7679a88153c3
2300032507 <163809251+2300032507@users.noreply.github.com>
2024-07-26 12:44:49 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,17 +3,17 @@ const CONFIG = {
 
     background: "background.jpg",
 
-    min1: "0Z5A6223.JPG",
+    min1: ".JPG",
     min2: "0Z5A6273.JPG",
     min3: "0Z5A6427.JPG",
     min4: "0Z5A6434.JPG",
     min5: "0Z5A6637.JPG",
     min6: "0Z5A6700.jpg",
 
-    max1: "m6.JPG",
-    max2: "m1.jpg",
-    max3: "m10.jpg",
-    max4: "m4.jpg",
-    max5: "m5.jpg",
-    max6: "m2.jpg",
+    max1: "s1.JPG",
+    max2: "s2.jpg",
+    max3: "s3.jpg",
+    max4: "s4.jpg",
+    max5: "s5.jpg",
+    max6: "s2.jpg",
 }
